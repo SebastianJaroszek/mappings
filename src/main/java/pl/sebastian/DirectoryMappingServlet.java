ad748a7701abcc4a3d0f753cadcace2a2a9443c4
SebastianJaroszek <sebastian_jaroszek@emstories.pl>
2018-02-16 18:29:06 +0100

    refactor and add DirectoryMappingServlet

--- src/main/java/pl/sebastian/DirectoryMappingServlet.java
+++ src/main/java/pl/sebastian/DirectoryMappingServlet.java
@@ -1,5 +1,8 @@
 package pl.sebastian;
 
+import sun.misc.Request;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,16 +10,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = {"/welcome", "/double/hello", "/hi"})
-public class ExactMatchingServlet extends HttpServlet {
+@WebServlet("/one/directory/two/*")
+public class DirectoryMappingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
-        resp.setCharacterEncoding("utf-8");
-
+        resp.setCharacterEncoding("UTF-8");
         PrintWriter out = resp.getWriter();
         out.println(RequestToHtmlConverter.convert(req));
-    }
 
+        String requestUri = req.getRequestURI();
+        String suffix = requestUri.replaceFirst(req.getContextPath() + "/one/directory/two/", "");
+        out.println("<br>End of URI: " + suffix);
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,14 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>servlet template</h2>
+Lista odnośników
+<ul>
+    <li><a href="welcome">welcome</a></li>
+    <li><a href="double/hello">double/hello</a></li>
+    <li><a href="hi">hi</a></li>
+    <li><a href="directory/movie.mp4">directory/movie.mp4</a></li>
+    <li><a href="2018/01/track01.mp3">2018/01/track01.mp3</a></li>
+    <li><a href="2018/01/track02.mp3">2018/01/track02.mp3</a></li>
+</ul>
 </body>
 </html>
